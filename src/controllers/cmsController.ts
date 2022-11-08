42c94220896199456fe9f72cebf41c868d6856ad
Peter Souza <peter.souza@farfetch.com>
2022-11-08 21:05:41 +0000

    feat: use the user locale to fetch the cms data

--- src/controllers/cmsController.ts
+++ src/controllers/cmsController.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 
 import * as cmsService from '../services/cmsService';
 
-export const landingPage = async (_: Request, res: Response, next: NextFunction) => {
+export const landingPage = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const response = await cmsService.landingPage();
+    const locale = req.query.locale as string;
+    const response = await cmsService.landingPage(locale || 'en');
     res.json(response);
   } catch (error: any) {
     next(error);
